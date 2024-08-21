89dea068ab1a3a3c39792d78b51f78a0e4bded10
dshlepov <158090880+dshlepov@users.noreply.github.com>
2024-08-22 00:53:36 +0300

    Update conftest.py

--- Lesson_9/conftest.py
+++ Lesson_9/conftest.py
@@ -1,10 +1,1 @@
-import pytest
-import requests # type: ignore
-from Lesson_9.conftest import url
-
-@pytest.fixture()
-def get_token(username='raphael' , password='cool-but-crude'):
-    log_pass = {'username': username, 'password': password}
-    resp_token = requests.post(url + '/auth/login', json=log_pass)
-    token = resp_token.json()['userToken']
-    return token
+url = "https://x-clients-be.onrender.com/"
