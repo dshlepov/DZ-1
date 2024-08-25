8265ebd8ac63f717109772080a5d900857b1534d
dshlepov <shik04041994@gmail.com>
2024-08-25 22:58:23 +0300

    Задание 9

--- Lesson_9/Pages/DataBase.py
+++ Lesson_9/Pages/DataBase.py
@@ -1,28 +1,26 @@
-from sqlalchemy import create_engine, text
+from sqlalchmy import create_engine, text # type: ignore
 
 class DataBase:
-    """Данная функциясоздает словарь query, где каждый ключ представляет тип запроса к базе данных (например, создание компании,
-    удаление компании и т.д.), а каждое значение - объект текстового запроса SQL, соответствующего этому типу запроса. Таким оброзом,
-    этот словарь содержит предварительно определенные SQL-запросы, которые будут использоваться для выполнения операций с базой данных."""
     query = {
-        'create_company': text('insert info company (name, description) values (: name, :description)'),
+        'create_company': text('insert into company (name, description) values (:name, : description)'),
         'max_company_id': text('select MAX(id) from company'),
         'delete_company': text('delete from company where id = :company_id'),
         'list_SELECT': text('select * from employee where company_id = :id'),
-        'item_SELECT': text('select * from employee where company_id = : c_id and id = :e_id'),
+        'item_SELECT': text('select * from employee where company_id = :c_id and id = :e_id'),
         'maxID_SELECT': text('select MAX(id) from employee where company_id = :c_id'),
         'item_DELETE': text('delete from employee where id = :id_delete'),
-        'item_UPDATE': text('update employee set first_name = :new_name where id = :employer_id'),
-        'item_INSERT': text('insert info employee(company_id, first_name, last_name, phone) values(:id, :name, :surname, :phone_nam)')
+        'item_UPDATE': text('update employeeset first_name = :new_name where id = :employer_id'),
+        'item_INSERT': text('insert into employee(company_id, first_name, last_name, phone) values(:id, :name, :surname, :phone_num)')
             }
-
-    def __init__(self, engine):
+    
+    def __init__(self, engine) -> None:
         self.db = create_engine(engine)
-
-    def execute_query(self, query_key, parameters=None):
+    
+    """Создаем компанию в БД"""
+    def create_company(self, company_name: str, description: str):
         try:
             with self.db.connect() as connection:
-                result = connection.execute(self.query[query_key], parameters)
+                result = connection.execute(self.query['create_company'], parameters=dict(name=company_name, description=description))
                 connection.commit()
                 return result
         except Exception as _ex:
@@ -32,29 +30,96 @@ class DataBase:
                 connection.close()
                 print("[INFO] DB connection closed")
     
-    def create_company(self, company_name: str, description: str):
-        return self.execute_query('create_company', {'name': company_name, 'description': description})
-    
+    """Удаляем компанию в БД"""
     def delete(self, company_id: int):
-        return self.execute_query('delete_company', {'company_id': company_id})
+        try:
+            with self.db.connect() as connection:
+                connection.execute(self.query['delete_company'], parameters=dict(company_id=company_id))
+                connection.commit()
+        except Exception as _ex:
+            print("[INFO] Error - can't work with SQL", _ex)
+        finally:
+            if connection:
+                connection.close()
+                print("[INFO] DB connection closed")
     
+    """Получаем ID последней созданной помпании"""
     def last_company_id(self):
-        result = self.execute_query('max_company_id')
-        return result.fetchall()[0][0]
+        try:
+            with self.db.connect() as connection:
+                result = connection.execute(self.query['max_company_id']).fetchall()[0][0]
+                return result
+        except Exception as _ex:
+            print("[INFO] Error - can't work with SQL", _ex)
+        finally:
+            if connection:
+                connection.close()
+                print("[INFO] DB connection closed")
     
-    def get_list_employer(self, company_id: int):
-        result = self.execute_query('list_SELECT', {'id': company_id})
-        return result.fetchall()
+    """Получаем список сотрудников из БД"""
+    def gett_list_employer(self, company_id: int):
+        try:
+            with self.db.connect() as connection:
+                result = connection.execute(self.query['list_SELECT'], parameters=dict(id=company_id)).fetchall()[0][0]
+                return result
+        except Exception as _ex:
+            print("[INFO] Error - can't work with SQL", _ex)
+        finally:
+            if connection:
+                connection.close()
+                print("[INFO] DB connection closed")
     
-    def create_employer(self, company_id: int, first_name: str, phone: str):
-        return self.execute_query('item_INSERT', {'id', company_id, 'name': first_name, 'surname': last_name, 'phone_nam': phone })
+    """Создаем сотрудника в БД"""
+    def create_employer(self, company_id: int, first_name: str, last_name: str, phone: str):
+        try:
+            with self.db.connect() as connection:
+                result = connection.execute(self.query['item_INSERT'], parameters=dict(id=company_id, name=first_name, surname=last_name,
+                                                                                       phone_num=phone))
+                connection.commit()
+                return result
+        except Exception as _ex:
+            print("[INFO] Error - can't work with SQL", _ex)
+        finally:
+            if connection:
+                connection.close()
+                print("[INFO] DB connection closed")
     
+    """Получаем ID сотрудника из БД"""
     def get_employer_id(self, company_id: int):
-        result = self.execute_query('maxID_SELECT', {'c_id': company_id})
-        return result.fetchall()[0][0]
+        try:
+            with self.db.connect() as connection:
+                result = connection.execute(self.query['maxID_SELECT'], parameters=dict(c_id=company_id)).fetchall()[0][0]
+                return result
+        except Exception as _ex:
+            print("[INFO] Error - can't work with SQL", _ex)
+        finally:
+            if connection:
+                connection.close()
+                print("[INFO] DB connection closed")
     
+    """Изменяем информацию о сотруднике в БД"""
     def update_employer_info(self, new_name: str, id: int):
-        return self.execute_query('item_UPDATE', {'new_name': new_name, 'employer_id': id})
+        try:
+            with self.db.connect() as connection:
+                result = connection.execute(self.query['item_UPDATE'], parameters=dict(new_name=new_name, employer_id=id))
+                connection.commit()
+        except Exception as _ex:
+            print("[INFO] Error - can't work with SQL", _ex)
+        finally:
+            if connection:
+                connection.close()
+                print("[INFO] DB connection closed")
     
+    """Удаляем сотрудника из БД"""
     def delete_employer(self, id: int):
-        return self.execute_query('item_delete', {'id_delete': id})
+        try:
+            with self.db.connect() as connection:
+                result = connection.execute(self.query['item_DELETE'], parameters=dict(id_selete=id))
+                connection.commit()
+                return result
+        except Exception as _ex:
+            print("[INFO] Error - can't work with SQL", _ex)
+        finally:
+            if connection:
+                connection.close()
+                print("[INFO] DB connection closed")
--- Lesson_9/Test/test_x_clients.py
+++ Lesson_9/Test/test_x_clients.py
@@ -3,59 +3,54 @@ from Lesson_9.Pages.Employee import Employer
 from Lesson_9.Pages.DataBase import DataBase
 
 api = Employer("https://x-clients-be.onrender.com")
-db = DataBase(
-    "postgresql+psycopg2://x_clients_user:95PM5lQE0NfzJWDQmLjbZ45ewrz1fLYa@dpg-cqsr9ulumphs73c2q40g-a.frankfurt-postgres.render.com/x_clients_db_fxd0")
+db = DataBase("postgresql://x_clients_user:95PM5lQE0NfzJWDQmLjbZ45ewrz1fLYa@dpg-cqsr9ulumphs73c2q40g-a.frankfurt-postgres.render.com/x_clients_db_fxd0")
 
-# Получаем список сотрудников из БД и АПИ, после чего сравниваем их
 def test_get_list_of_employers():
     db.create_company('Evgen testers', 'cool_company')
     max_id = db.last_company_id()
     db.create_employer(max_id, "Evgen", "Voronov", "8002000600")
-    db_employer_list = db.get_list_employer(max_id)
-    api_employer_list = api.get_list(max_id)
+    db_employer_list = db.gett_list_employer(max_id)
+    api_employer_list =  api.get_list(max_id)
     assert len(db_employer_list) == len(api_employer_list)
     response = (api.get_list(max_id))[0]
     employer_id = response["id"]
     db.delete_employer(employer_id)
     db.delete(max_id)
 
-# Добавляем сотрудника в БД и сравниваем с АПИ имя, статус и фамилию
-def test_add_new_employer():
-    db.create_company('Evgen adding new employer', 'employer')
-    max_id = db.last_company_id()
-    db.create_employer(max_id, "Evgen", "Voronov", "8002000600")
-    response = (api.get_list(max_id))[0]
-    employer_id = response["id"]
-    assert response["companyId"] == max_id
-    assert response["firctName"] == "Evgen"
-    assert response["isActive"] == True
-    assert response["lastName"] == "Voronov"
-    db.delete_employer(employer_id)
-    db.delete(max_id)
+    def test_add_new_employer():
+        db.create_company('Evgen adding new employer', 'employer')
+        max_id = db.last_company_id()
+        db.create_employer(max_id, "Evgen", "Voronov", "8002000600")
+        response = (api.get_list(max_id))[0]
+        employer_id = response["id"]
+        assert response['companyId'] == max_id
+        assert response["firstName"] == "Evgen"
+        assert response["isActive"] == True
+        assert response["lastName"] == "Voronov"
+        db.delete_employer(employer_id)
+        db.delete(max_id)
 
-# Сравниваем информацию о сотруднике полученную по API с информацией указанной при создании сотрудника в БД
-def test_assertion_data():
-    db.create_company('Employer get id company', 'new')
-    max_id = db.last_company_id ()
-    db.create_employer(max_id, "Evgen", "Voronov", "8002000600")
-    employer_id = db.get_employer_id(max_id)
-    get_api_info = (api.get_info(employer_id)).json()
-    assert get_api_info["firstName"] == "Evgen"
-    assert get_api_info["lastName"] == "Voronov"
-    assert get_api_info["phone"] == "8002000600"
-    db.delete_employer(employer_id)
-    db.delete(max_id)
+    def test_assertion_data():
+        db.create_company('Employer get id compaany', 'new')
+        max_id = db.last_company_id()
+        db.create_employer(max_id, "Evgen", "Voronov", "8002000600")
+        employer_id = db.get_employer_id(max_id)
+        get_api_info = (api.get_info(employer_id)).json()
+        assert get_api_info["firstName"] == "Evgen"
+        assert get_api_info["lastName"] == "Voronov"
+        assert get_api_info["phone"] == "8002000600"
+        db.delete_employer(employer_id)
+        db.delete(max_id)
 
-# Сравниваем информацию о сотруднике полученную по АПИ с измененной информациейв БД информацией о сотруднике
-def test_update_user_info():
-    db.create_company('New updating company', 'test')
-    max_id = db.last_company_id()
-    db.create_employer(max_id, "Evgen", "Voroniv", "8002000600")
-    employer_id =db.get_employer_id(max_id)
-    db.update_employer_info("King", employer_id)
-    get_api_info = (api.get_info(employer_id)).json()
-    assert get_api_info["firstName"] == "King"
-    assert get_api_info["lastName"] == "Voronov"
-    assert get_api_info["isActive"] == True
-    db.delete_employer(employer_id)
-    db.delete(max_id)
+        def test_update_user_info():
+            db.create_company('New updating company', 'test')
+            max_id = db.last_company_id()
+            db.create_employer(max_id, "Evgen", "Voronov", "8002000600")
+            employer_id = db.get_employer_id(max_id)
+            db.update_employer_info("King", employer_id)
+            get_api_info = (api.get_info(employer_id)).json()
+            assert get_api_info["firstName"] == "King"
+            assert get_api_info["lastName"] == "Voronov"
+            assert get_api_info["isActive"] == True
+            db.delete_employer(employer_id)
+            db.delete(max_id)
